a0f58b69dd3c531135f881b35478f5171ec8530b
Dax <dax86it@gmail.com>
2012-06-09 21:56:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sublime Text 2 plugin: XPathFind
+
+XPathFind will search the current view for xml nodes matching a particular xpath expression.
+Run "x_path_find" command and you will be prompted to enter the expression via the input panel.
+
+## Requirements
+
+This plugin uses the lxml library for xpath search.
+<pre>easy_install lxml</pre> should install it.
+
+## Install
+
+Clone this repository as XPathFind into your Packages directory.
---
+++ xpath-find.py
@@ -0,0 +1,56 @@
+import sublime, sublime_plugin
+
+class XPathFindCommand(sublime_plugin.TextCommand):
+
+	xml_tree = None
+	query_last = ""
+
+	def run(self, edit):
+		window = self.view.window()
+		window.show_input_panel("XPath", self.query_last, self.on_input_done, self.on_input_change, self.on_input_cancel)
+
+	def on_input_done(self, value):
+		self.query_last = value
+		self.xpath_find(value)
+
+	def on_input_change(self, value):
+		self.query_last = value
+		self.xpath_find(value, False)
+		pass
+
+	def on_input_cancel(self, value):
+		# Free memory
+		self.xml_tree = None
+		pass
+
+	def xpath_find(self, expr, parse = True):
+
+		if parse or not self.xml_tree:
+			xml_raw = self.view.substr(sublime.Region(0, self.view.size()))
+
+			from lxml import etree
+			from StringIO import StringIO
+
+			xml_raw_io = StringIO(xml_raw)
+
+			try:
+				self.xml_tree = etree.parse(xml_raw_io)
+			except Exception as e:
+				sublime.error_message(str(e.message))
+
+		nodes = self.xml_tree.xpath(expr)
+
+		sublime.status_message("XPath: found {0} match(es) for {1}".format(len(nodes), expr))
+
+		self.view.sel().clear()
+
+		for node in nodes:
+			pt = self.view.text_point(node.sourceline - 1, 0)
+			self.view.sel().add(sublime.Region(pt))
+			self.view.show(pt)
+			self.view.run_command("expand_selection", {'to': 'line'})
+
+		return nodes
+
+
+
