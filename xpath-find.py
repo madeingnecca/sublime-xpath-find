ec2d46a7c59840257216a7bac1c1117701d901fa
Damiano Seno <dax86it@gmail.com>
2012-12-18 21:28:34 +0100

    almost pep8 compliant + support for non-list result

--- xpath-find.py
+++ xpath-find.py
@@ -1,54 +1,57 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+from lxml import etree
+from StringIO import StringIO
+
 
 class XPathFindCommand(sublime_plugin.TextCommand):
 
-	xml_tree = None
-	query_last = ""
+    xml_tree = None
+    query_last = ''
 
-	def run(self, edit):
-		window = self.view.window()
-		window.show_input_panel("XPath", self.query_last, self.on_input_done, self.on_input_change, self.on_input_cancel)
+    def run(self, edit):
+        window = self.view.window()
+        window.show_input_panel('XPath',
+                                self.query_last,
+                                self.on_input_done,
+                                self.on_input_change, self.on_input_cancel)
 
-	def on_input_done(self, value):
-		self.query_last = value
-		self.xpath_find(value)
+    def on_input_done(self, value):
+        self.query_last = value
+        self.xpath_find(value)
 
-	def on_input_change(self, value):
-		self.query_last = value
-		self.xpath_find(value, False)
+    def on_input_change(self, value):
+        self.query_last = value
+        self.xpath_find(value, False)
 
-	def on_input_cancel(self, value):
-		# Free memory
-		self.xml_tree = None
+    def on_input_cancel(self):
+        # Save memory
+        self.xml_tree = None
 
-	def xpath_find(self, expr, parse = True):
+    def xpath_find(self, expr, parse=True):
 
-		if parse or not self.xml_tree:
-			xml_raw = self.view.substr(sublime.Region(0, self.view.size()))
+        if parse or not self.xml_tree:
+            xml_raw = self.view.substr(sublime.Region(0, self.view.size()))
+            xml_raw_io = StringIO(xml_raw)
 
-			from lxml import etree
-			from StringIO import StringIO
+            try:
+                self.xml_tree = etree.parse(xml_raw_io)
+            except Exception as e:
+                sublime.error_message(str(e.message))
 
-			xml_raw_io = StringIO(xml_raw)
+        result = self.xml_tree.xpath(expr)
 
-			try:
-				self.xml_tree = etree.parse(xml_raw_io)
-			except Exception as e:
-				sublime.error_message(str(e.message))
+        result_type = type(result)
 
-		nodes = self.xml_tree.xpath(expr)
+        if result_type is list:
+            sublime.status_message('XPath: found {0} match(es) for {1}'.format(len(result), expr))
 
-		sublime.status_message("XPath: found {0} match(es) for {1}".format(len(nodes), expr))
+            self.view.sel().clear()
 
-		self.view.sel().clear()
-
-		for node in nodes:
-			pt = self.view.text_point(node.sourceline - 1, 0)
-			self.view.sel().add(sublime.Region(pt))
-			self.view.show(pt)
-			self.view.run_command("expand_selection", {'to': 'line'})
-
-		return nodes
-
-
-
+            for node in result:
+                pt = self.view.text_point(node.sourceline - 1, 0)
+                self.view.sel().add(sublime.Region(pt))
+                self.view.show(pt)
+                self.view.run_command('expand_selection', {'to': 'line'})
+        else:
+            sublime.status_message('XPath result: {0} for {1}'.format(result, expr))
