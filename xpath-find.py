f23e8a09021019b96d14b449bc5711113ded9e14
Damiano Seno <dax86it@gmail.com>
2013-07-31 22:43:42 +0200

    bug fixing + improved matches highlight

--- xpath-find.py
+++ xpath-find.py
@@ -5,7 +5,6 @@ from StringIO import StringIO
 
 
 class XPathFindCommand(sublime_plugin.TextCommand):
-
     xml_tree = None
     query_last = ''
 
@@ -25,11 +24,10 @@ class XPathFindCommand(sublime_plugin.TextCommand):
         self.xpath_find(value, False)
 
     def on_input_cancel(self):
-        # Save memory
+        # When canceling, forget the parsed tree.
         self.xml_tree = None
 
     def xpath_find(self, expr, parse=True):
-
         if parse or not self.xml_tree:
             xml_raw = self.view.substr(sublime.Region(0, self.view.size()))
             xml_raw_io = StringIO(xml_raw)
@@ -38,20 +36,38 @@ class XPathFindCommand(sublime_plugin.TextCommand):
                 self.xml_tree = etree.parse(xml_raw_io)
             except Exception as e:
                 sublime.error_message(str(e.message))
+                return
 
-        result = self.xml_tree.xpath(expr)
+        self.view.sel().clear()
+
+        try:
+            result = self.xml_tree.xpath(expr)
+        except Exception as e:
+            sublime.status_message(str(e.message))
+            return
 
         result_type = type(result)
 
         if result_type is list:
             sublime.status_message('XPath: found {0} match(es) for {1}'.format(len(result), expr))
 
-            self.view.sel().clear()
-
+            regions = []
             for node in result:
-                pt = self.view.text_point(node.sourceline - 1, 0)
-                self.view.sel().add(sublime.Region(pt))
+                # Look for the first character of the tag
+                # and add it to the selection.
+                # The "tag" package will highlight
+                # where it starts and where it ends.
+                i = 0
+                while True:
+                    pt = self.view.text_point(node.sourceline - 1, i)
+                    pt_str = self.view.substr(pt)
+                    i += 1
+                    if pt_str != ' ' and pt_str != '\t':
+                        break
+
+                pt = self.view.text_point(node.sourceline - 1, i)
+                r = sublime.Region(pt)
+                self.view.sel().add(r)
                 self.view.show(pt)
-                self.view.run_command('expand_selection', {'to': 'line'})
         else:
             sublime.status_message('XPath result: {0} for {1}'.format(result, expr))
